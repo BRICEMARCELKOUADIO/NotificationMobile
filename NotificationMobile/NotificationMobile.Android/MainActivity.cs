31eaba96ddbc13f6d914fd882b7a1b013f9355ec
Brice Marcel <kbricemarcel@outlook.fr>
2020-11-09 16:06:56 +0000

    Configuration du fichier Google Json

--- NotificationMobile/NotificationMobile.Android/MainActivity.cs
+++ NotificationMobile/NotificationMobile.Android/MainActivity.cs
@@ -6,6 +6,7 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Plugin.FirebasePushNotification;
 
 namespace NotificationMobile.Droid
 {
@@ -18,7 +19,7 @@ namespace NotificationMobile.Droid
             ToolbarResource = Resource.Layout.Toolbar;
 
             base.OnCreate(savedInstanceState);
-
+            FirebasePushNotificationManager.Initialize(this, true);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
