e59a2ea219f112cfff802b829ed6473cc97d1c90
Brice Marcel <kbricemarcel@outlook.fr>
2020-11-09 14:30:23 +0000

    Ajoutez des fichiers projet.

---
+++ NotificationMobile/NotificationMobile/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using NotificationMobile.ViewModels;
+
+namespace NotificationMobile.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ NotificationMobile/NotificationMobile/Views/NewItemPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using NotificationMobile.Models;
+using NotificationMobile.ViewModels;
+
+namespace NotificationMobile.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
